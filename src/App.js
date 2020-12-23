1da7f88c696770580676754c2f1b052e7e736ea6
Ahmet Topic <ahmettopic@hotmail.com>
2020-12-23 22:09:19 +0100

    update

--- src/App.js
+++ src/App.js
@@ -3,24 +3,9 @@ import React, { Component } from 'react';
 
 import BowlingPins from './components/BowlingPins';
 
-const styles = {
-  width: '100%',
-  display: 'flex',
-  height: '5vh',
-  justifyContent: 'center',
-  textAlign: 'center',
-  fontSize: '32px',
-  fontFamily: 'Fira Code',
-};
-
 class App extends Component {
   render() {
-    return (
-      <div>
-        <div style={styles}>BowlingScoreChallenge</div>
-        <BowlingPins />
-      </div>
-    );
+    return <BowlingPins />;
   }
 }
 
--- src/components/BowlingPins.js
+++ src/components/BowlingPins.js
@@ -2,33 +2,42 @@ import React, { Component } from 'react';
 import Pin from './Pin';
 class BowlingPins extends Component {
   state = {
-    defaultPinAmount: [10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
-    counter: 2,
+    defaultPinAmount: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    counter: 1, // update counter each turn
+    currentRound: 1, // something
+    totalScore: 0,
+    spare: false, // Check if player got spare
+    strike: false, // Check if player got Strike
     showPins: true,
-    nextRoundText: 'MISS',
   };
 
-  setCounter = () => {
-    console.log('counter: ' + this.state.counter);
-    if (this.state.counter <= 1) {
-      this.setState({ showPins: false });
-      this.setState({ nextRoundText: 'NEXT ROUND!' });
-      // RETURN COMPONENT HERE
-      return console.log('No more kast!');
+  checkCount = () => {
+    if (this.state.counter === 2) {
+      return this.setState(
+        {
+          showPins: false,
+          counter: this.state.counter - 2,
+          currentRound: this.state.currentRound + 1,
+        },
+        console.log('Round Finished!')
+      );
+
+      //RETURN COMPONENT HERE
     } else {
-      this.setState({ counter: this.state.counter + -1 });
+      return this.setState({ counter: this.state.counter + 1 });
     }
   };
 
   selectedPinValue = (i) => {
     this.setState((state) => {
-      const defaultPinAmount = state.defaultPinAmount.slice((0, i + 1));
+      const defaultPinAmount = state.defaultPinAmount.slice((1, i + 1));
+
       return {
         defaultPinAmount,
       };
     });
-
-    this.setCounter();
+    console.log('selectedPinAmount: ' + (i + 1));
+    this.checkCount();
   };
 
   render() {
@@ -41,7 +50,6 @@ class BowlingPins extends Component {
       textAlign: 'center',
       backgroundColor: '#2b2b2b',
     };
-
     return (
       <React.Fragment>
         <div style={styles}>
@@ -53,19 +61,23 @@ class BowlingPins extends Component {
               width: '50%',
               color: 'white',
             }}
-            onClick={this.setCounter}
           >
             {this.state.defaultPinAmount.map((item, index) => (
               <div
+                onMouseOver={this.selectedPinValueCOUNTER}
                 key={item}
                 type="button"
-                onClick={() => this.selectedPinValue(index)}
+                onClick={() =>
+                  this.selectedPinValue(index) &
+                  (this.state.counter + 1, this.state.totalScore + index)
+                }
               >
                 {this.state.showPins ? <Pin /> : null}
               </div>
             ))}
           </div>
         </div>
+        <button onClick={this.checkCount}>MISS</button>
       </React.Fragment>
     );
   }
